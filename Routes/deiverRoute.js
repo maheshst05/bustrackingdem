04cf3162ae8b5b9b8db45c5986a6796691110bb8
maheshst05 <maheshthombare005.com>
2023-09-01 18:05:57 +0530

    update code 1

--- Routes/deiverRoute.js
+++ Routes/deiverRoute.js
@@ -49,6 +49,7 @@ driverRouter.post("/api/driver/reg", async (req, res) => {
 driverRouter.post("/api/driver/login", async (req, res) => {
   try {
     const { phoneNo, password, grant_type } = req.body;
+
     if (grant_type !== "password") {
       return res.status(400).json({ msg: "Invalid grant_type", status: false });
     }
@@ -56,38 +57,37 @@ driverRouter.post("/api/driver/login", async (req, res) => {
     // Find the user by phone number
     const user = await Driver.findOne({ phoneNo });
     if (!user) {
-      return res
-        .status(401)
-        .json({ msg: "Invalid credentials", status: false });
+      return res.status(401).json({ msg: "Invalid credentials", status: false });
     }
 
     // Compare passwords
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
-      return res
-        .status(401)
-        .json({ msg: "Invalid credentials", status: false });
+      return res.status(401).json({ msg: "Invalid credentials", status: false });
     }
 
-    // Generate an access token
+    // Generate an access token with a 7-hour expiry
     const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "7h",
+      expiresIn: "7h", // 7 hours
     });
+
+    // Store the access token if needed (optional)
     const accessTokenstore = new Token({
       userId: user._id,
       tokenType: "access_token",
       tokenValue: accessToken,
     });
     await accessTokenstore.save();
-    // Generate a refresh token
+
+    // Generate a refresh token with a 7-day expiry
     const refreshToken = jwt.sign(
       { userId: user._id },
       process.env.RefreshToken,
-      { expiresIn: "7d" }
+      { expiresIn: "7d" } // 7 days
     );
 
     return res.status(200).json({
-      msg: "login successfully",
+      msg: "Login successfully",
       status: true,
       accessToken,
       refreshToken,
--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -42,63 +42,50 @@ userRouter.post("/api/auth/register", async (req, res) => {
     return res.status(500).json({ msg: "An error occurred", status: false });
   }
 });
-
 userRouter.post("/api/auth/login", async (req, res) => {
   try {
-    const { phoneNo, password , grant_type } = req.body;
-if( grant_type !=='password' ){
-  return res
-  .status(400)
-  .json({ msg: "Invalid grant_type", status: false });
-}
+    const { phoneNo, password, grant_type } = req.body;
+
+    if (grant_type !== 'password') {
+      return res.status(400).json({ msg: "Invalid grant_type", status: false });
+    }
 
     // Find the user by phone number
     const user = await User.findOne({ phoneNo });
     if (!user) {
-      return res
-        .status(401)
-        .json({ msg: "Invalid credentials", status: false });
+      return res.status(401).json({ msg: "Invalid credentials", status: false });
     }
 
     // Compare passwords
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
-      return res
-        .status(401)
-        .json({ msg: "Invalid credentials", status: false });
+      return res.status(401).json({ msg: "Invalid credentials", status: false });
     }
 
-    // Generate an access token
+    // Generate an access token with a 1-day expiry (24 hours)
     const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1d",
     });
-// const accessTokenstore = new Token({
-//     userId:user._id,
-//     tokenType:"access_token",
-//     tokenValue:accessToken,
-// })
-//await accessTokenstore.save()
 
-    // Generate a refresh token
+    // Generate a refresh token with a 7-day expiry
     const refreshToken = jwt.sign(
       { userId: user._id },
       process.env.RefreshToken,
       { expiresIn: "7d" }
     );
 
-    return res
-      .status(200)
-      .json({
-        msg: "login successfully",
-        status: true,
-        accessToken,
-        refreshToken,
-      });
+    return res.status(200).json({
+      msg: "Login successfully",
+      status: true,
+      accessToken,
+      refreshToken,
+    });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ msg: "An error occurred", status: false });
   }
 });
+
 
 userRouter.post("/api/auth/logout",authentication, async (req, res) => {
     console.log(req.body)
