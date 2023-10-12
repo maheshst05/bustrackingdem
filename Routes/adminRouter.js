b30932c82616a9e3fac03da7ce933abceb155d0a
maheshst05 <maheshthombare005.com>
2023-10-12 11:44:22 +0530

    update

--- Routes/adminRouter.js
+++ Routes/adminRouter.js
@@ -8,54 +8,13 @@ const Country = require("../Model/countryModel");
 const City = require("../Model/CityModel");
 const bcrypt = require("bcrypt");
 
-//drivers
-// AdminRouter.get("/api/get/drivers/", async (req, res) => {
-//   try {
-//     const  isvisible  = req.query.isvisible;
-//     const search = req.query.search;
+//**Driver**
 
-//     const filter = {
-//       profileType: "Driver",
-//     };
-
-//     if (search) {
-//       filter.$or = [
-//         { name: { $regex: search, $options: "i" } },
-//         { "address.country.countryName": { $regex: search, $options: "i" } },
-//         { "address.city": { $regex: search, $options: "i" } },
-//       ];
-//     }
-
-//     let managers;
-
-//     if (isvisible === "true") {
-//       managers = await User.find(filter)
-//         .select("name id licenceNo dob phoneNo email address calling")
-//         .lean();
-//     } else {
-//       const isAssignedDriverIds = await BusRoute.distinct(
-//          "driver_details._id"
-//     );
-
-//       filter._id = { $nin: isAssignedDriverIds };
-
-//       managers = await User.find(filter)
-//         .select("name id licenceNo dob phoneNo email address calling")
-//         .lean();
-//     }
-
-//     return res.status(200).json({ managers });
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({ msg: "Internal server error" });
-//   }
-// });
-
-
+//get driver
 AdminRouter.get("/api/get/drivers/:city?", async (req, res) => {
   try {
-     const city = req.params.city;
-    const  isvisible  = req.query.isvisible;
+    const city = req.params.city;
+    const isvisible = req.query.isvisible;
     const search = req.query.search;
 
     const filter = {
@@ -79,7 +38,7 @@ AdminRouter.get("/api/get/drivers/:city?", async (req, res) => {
     } else {
       const isAssignedDriverIds = await BusRoute.distinct("driver_details._id");
       filter._id = { $nin: isAssignedDriverIds };
-      filter["address._id"] = city; 
+      filter["address._id"] = city;
       managers = await User.find(filter)
         .select("name id licenceNo dob phoneNo email address calling")
         .lean();
@@ -91,23 +50,23 @@ AdminRouter.get("/api/get/drivers/:city?", async (req, res) => {
   }
 });
 
-
 //update driver
 AdminRouter.put("/api/update/driver/:id", async (req, res) => {
   const id = req.params.id;
   try {
-    // Check if the request body contains a password field
+   
     if (req.body.password) {
-      // Hash the new password
       req.body.password = await bcrypt.hash(req.body.password, 10);
+
+    }
+    if (!req.body.password) {
+      delete req.body.whoisUpdate;
     }
 
     // Update the user document
-    const driver = await User.findByIdAndUpdate(
-      { _id: id },
-      req.body,
-      { new: true } 
-    );
+    const driver = await User.findByIdAndUpdate({ _id: id }, req.body, {
+      new: true,
+    });
 
     if (!driver) {
       return res.status(404).json({ message: "Driver not found" });
@@ -152,7 +111,6 @@ AdminRouter.post("/api/add/bus", async (req, res) => {
       .json({ message: "Bus added successfully", bus: newBus });
   } catch (error) {
     console.error(error);
-    //return res.status(400).json( "Route is already Presemt" );
   }
 });
 
@@ -198,44 +156,9 @@ AdminRouter.delete("/api/delete/bus/:id", async (req, res) => {
 });
 
 //get buses
-// AdminRouter.get("/api/get/buses/", async (req, res) => {
-//   const city = req.params.city
-//   const  isvisible  = req.query.isvisible;
-//   const search = req.query.search;
-//   try {
-//     let filter = {};
-//     if (search) {
-//       filter = {
-//         $or: [
-//           { busNo: { $regex: search, $options: "i" } },
-//           { "address.country.countryName": { $regex: search, $options: "i" } },
-//           { "address.city": { $regex: search, $options: "i" } },
-//         ],
-//       };
-//     }
-
-//     let Buses;
-
-//     if (isvisible === "true") {
-//       Buses = await Bus.find(filter);
-//     } else {
-//       const assignedBuses = await BusRoute.find().select("bus_details._id");
-//       const ids = assignedBuses.map((item) => item.bus_details._id);
-//       Buses = await Bus.find({ _id: { $nin: ids }, ...filter });
-//     }
-
-//     return res.status(200).json({ Buses });
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({ msg: "Internal server error" });
-//   }
-// });
-
-
-
 AdminRouter.get("/api/get/buses/:city?", async (req, res) => {
-  const city = req.params.city
-  const  isvisible  = req.query.isvisible;
+  const city = req.params.city;
+  const isvisible = req.query.isvisible;
   const search = req.query.search;
   try {
     let filter = {};
@@ -256,14 +179,10 @@ AdminRouter.get("/api/get/buses/:city?", async (req, res) => {
     } else {
       const assignedBuses = await BusRoute.find().select("bus_details._id");
       const ids = assignedBuses.map((item) => item.bus_details._id);
-      
-Buses = await Bus.find({
-  $and: [
-    { _id: { $nin: ids } },
-    { "address._id": city },
-    filter,
-  ],
-});
+
+      Buses = await Bus.find({
+        $and: [{ _id: { $nin: ids } }, { "address._id": city }, filter],
+      });
     }
 
     return res.status(200).json({ Buses });
@@ -272,12 +191,6 @@ Buses = await Bus.find({
     return res.status(500).json({ msg: "Internal server error" });
   }
 });
-
-
-
-
-
-
 
 //Route
 //add routes
@@ -310,7 +223,6 @@ AdminRouter.put("/api/update/route/:id", async (req, res) => {
 AdminRouter.delete("/api/delete/route/:id", async (req, res) => {
   const id = req.params.id;
   try {
-   
     const deletedRoute = await Route.findByIdAndDelete({ _id: id });
     return res.status(200).json({ message: " Route Deleted successfully" });
   } catch (error) {
@@ -321,10 +233,9 @@ AdminRouter.delete("/api/delete/route/:id", async (req, res) => {
 //get routes
 AdminRouter.get("/api/get/routes/:city?", async (req, res) => {
   try {
-    const city = req.params.city
-    const search  =req.query.search
-    const filter = {
-    };
+    const city = req.params.city;
+    const search = req.query.search;
+    const filter = {};
 
     if (search) {
       filter.$or = [
@@ -335,9 +246,10 @@ AdminRouter.get("/api/get/routes/:city?", async (req, res) => {
       ];
       const routes = await Route.find(filter);
       return res.status(200).json({ routes });
-    }if(city){
-      const routes = await Route.find({"address._id": city});
-      return res.status(200).json({ routes })  
+    }
+    if (city) {
+      const routes = await Route.find({ "address._id": city });
+      return res.status(200).json({ routes });
     }
     const routes = await Route.find();
     return res.status(200).json({ routes });
@@ -400,8 +312,6 @@ AdminRouter.get("/api/get/busroute", async (req, res) => {
   }
 });
 
-
-
 AdminRouter.get("/api/search/source/destination/:token?", async (req, res) => {
   const { sourceRoute, destinationRoute } = req.query;
 
@@ -423,7 +333,7 @@ AdminRouter.get("/api/search/source/destination/:token?", async (req, res) => {
       "route_details.destinationRoute": 1,
       "route_details.stops": 1,
       "route_details.polyline": 1,
-      "address":1,
+      address: 1,
       time: 1,
       status: 1,
       currentRouteLocation: 1,
@@ -656,39 +566,36 @@ AdminRouter.delete("/api/delete/country/:id", async (req, res) => {
 AdminRouter.get("/api/get/city/:country?", async (req, res) => {
   try {
     const country = req.params.country;
-    if(country){
-     const cityes = await City.find({
-      "country.countryName":country ,
-    });
+    if (country) {
+      const cityes = await City.find({
+        "country.countryName": country,
+      });
 
-    if (cityes.length === 0) {
-      return res.status(404).json({ msg: "NO city Found in this Country" });
+      if (cityes.length === 0) {
+        return res.status(404).json({ msg: "NO city Found in this Country" });
+      }
+
+      return res.status(200).json(cityes);
+    } else {
+      const cityes = await City.find();
+
+      if (cityes.length === 0) {
+        return res.status(404).json({ msg: "City not found" });
+      }
+
+      return res.status(200).json(cityes);
     }
-
-    return res.status(200).json(cityes);
-  }
-  else{
-    const cityes = await City.find();
-
-    if (cityes.length === 0) {
-      return res.status(404).json({ msg: "City not found" });
-    }
-
-    return res.status(200).json(cityes);
-  }
-
   } catch (error) {
     console.error(error);
     return res.status(500).json({ msg: "Internal server error" });
   }
 });
 
-
 //post city with country
 
 AdminRouter.post("/api/post/city", async (req, res) => {
   try {
-    const citys= new City(req.body);
+    const citys = new City(req.body);
     await citys.save();
     return res.status(200).json({ msg: "New city added successfully" });
   } catch (error) {
@@ -700,13 +607,13 @@ AdminRouter.post("/api/post/city", async (req, res) => {
 //update city with country
 AdminRouter.put("/api/update/city/:id", async (req, res) => {
   try {
-  const city = await City.findByIdAndUpdate(req.params.id, req.body)
-  return res.status(200).json({ msg: "City updated successfully" });
+    const city = await City.findByIdAndUpdate(req.params.id, req.body);
+    return res.status(200).json({ msg: "City updated successfully" });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ msg: "Internal server error" });
   }
-})
+});
 
 //delete city with country
 AdminRouter.delete("/api/delete/city/:id", async (req, res) => {
